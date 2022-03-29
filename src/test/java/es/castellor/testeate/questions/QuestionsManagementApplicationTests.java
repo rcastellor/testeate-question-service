db25048b9a485f03de7e56c2ecaf1234ef64ee03
Roberto Castellor <roberto.castellor@gmail.com>
2022-03-29 18:45:38 +0200

    Initial commit

---
+++ src/test/java/es/castellor/testeate/questions/QuestionsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package es.castellor.testeate.questions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
